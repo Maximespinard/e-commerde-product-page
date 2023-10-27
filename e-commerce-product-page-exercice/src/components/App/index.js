23d136f14fe4ba53c112506935efa9626f4e2bee
mspinard <mspinard@vulog.com>
2023-10-27 11:55:41 +0200

    install sass, husky

--- e-commerce-product-page-exercice/src/components/App/index.js
+++ e-commerce-product-page-exercice/src/components/App/index.js
@@ -1,17 +1,20 @@
 function App() {
   return (
     <>
-    <div className="App">
-      Collections Men Women About Contact Sneaker Company Fall Limited Edition Sneakers These low-profile sneakers are your perfect casual
-      wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer. $125.00 50% $250.00 0 Add to
-      cart
-    </div>
-        <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. Coded by
-        <a href="#">Your Name Here</a>.
+      <div className='App'>
+        Collections Men Women About Contact Sneaker Company Fall Limited Edition Sneakers These low-profile sneakers are your perfect casual
+        wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer. $125.00 50% $250.00 0 Add
+        to cart
+      </div>
+      <div class='attribution'>
+        Challenge by{' '}
+        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href='#'>Your Name Here</a>.
       </div>
     </>
-    
   );
 }
 
