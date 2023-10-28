cf1e7641b960ae821f188af55ddc840edd58529d
mspinard <mspinard@vulog.com>
2023-10-28 18:48:41 +0200

    ready to go

--- e-commerce-product-page-exercice/src/components/App/index.js
+++ e-commerce-product-page-exercice/src/components/App/index.js
@@ -1,7 +1,10 @@
+import Header from '../Header';
+
 function App() {
   return (
     <>
-          <div className='App'>
+      <Header />
+      {/*  <div className='App'>
         Collections Men Women About Contact Sneaker Company Fall Limited Edition Sneakers These low-profile sneakers are your perfect casual
         wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer. $125.00 50% $250.00 0 Add
         to cart
@@ -14,6 +17,7 @@ function App() {
         . Coded by
         <a href='#'>Your Name Here</a>.
       </div>
+ */}
     </>
   );
 }
